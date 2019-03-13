a304a99a330ae7ff6f5bc709bf7b25c9662298d8
Igor Carvalho <dev.igorcarvalho@gmail.com>
2019-03-13 20:17:49 -0300

    finalizando icons

--- src/app/services/data-source.service.ts
+++ src/app/services/data-source.service.ts
@@ -14,7 +14,7 @@ export class DataSourceService {
 
   localDataSource: Local[] = [
     {id: 1, title: 'Cozy Room 1', subTitle: '3 blocks away from subway', description: this.description, fotoUrl: '../../../../assets/images/local3.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Ipanema', rating: '',  distance: 9.2, time: 0, price: 6, extras: ''},
-    {id: 2, title: 'Cozy Room 2', subTitle: 'Near the supermarket', description: this.description, fotoUrl: '../../../../assets/images/local4.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Copacabana', rating: '',  distance: 3.6, time: 0, price: 2, extras: ''},
+    {id: 2, title: 'Cozy Room 2', subTitle: 'Near the supermarket', description: this.description, fotoUrl: '../../../../assets/images/local4.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Catete', rating: '',  distance: 3.6, time: 0, price: 2, extras: ''},
     {id: 3, title: 'Cozy Room 3', subTitle: 'Best place for a napp', description: this.description, fotoUrl: '../../../../assets/images/local2.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Tijuca', rating: '',  distance: 4.8, time: 0, price: 3, extras: ''},
     {id: 4, title: 'Cozy Room 4', subTitle: 'Best view ever', description: this.description, fotoUrl: '../../../../assets/images/local1.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Botafogo', rating: '',  distance: 6.9, time: 0, price: 4, extras: ''},
     {id: 5, title: 'Cozy Room 5', subTitle: 'Near a surfing spot', description: this.description, fotoUrl: '../../../../assets/images/local4.jpg', fotoMap: '../../assets/images/map.JPG', bairro: 'Centro', rating: '',  distance: 0.9, time: 0, price: 5, extras: ''},
